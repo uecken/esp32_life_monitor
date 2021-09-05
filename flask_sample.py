44e2233a4ad68b8c1a856de2080380eac5483fc0
uecken <aa>
2021-09-05 11:46:35 +0000

    changed some name

--- flask_sample.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-app = Flask(__name__,static_folder='.',static_url_path='.')
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
-
-app.run(port=80,debug=True)
